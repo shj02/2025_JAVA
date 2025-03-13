aaf7d720801142054ae0721664b2f9fccaca3f75
shj02 <163235571+shj02@users.noreply.github.com>
2025-03-13 15:04:08 +0900

    DataType4

---
+++ week02/DataType4.java
@@ -0,0 +1,24 @@
+package week2;
+
+public class DataType4 {
+	public static void main(String[] args) {
+		//byte = 1byte(-128 ~127)
+		byte	bData = 127;
+		
+		//char : 음수가 없다!
+		char	cData = 90;
+		 
+		float	fData = 3.14f;
+		
+		//정수 : int로 정의됨
+		long	lData = 10000000000L;
+		
+		boolean	boolD = true;
+		
+		System.out.println("bData = " + bData);
+		System.out.println("cData = " + cData);
+		System.out.println("fData = " + fData);
+		System.out.println("lData = " + lData);
+		System.out.println("boolD = " + boolD);
+	}
+}
