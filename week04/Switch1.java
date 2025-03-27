8e487e31e14459ea2e975fc9a5ee3368c6cd38c7
shj02 <163235571+shj02@users.noreply.github.com>
2025-03-27 14:52:23 +0900

    Switch

---
+++ week04/Switch1.java
@@ -0,0 +1,18 @@
+package week4;
+
+public class Switch1 {
+	public static void main(String[] args) {
+		char grade = 'A';
+		
+		switch(grade) {
+		case 'A': case 'a':
+			System.out.println("A 등급");
+			break;
+		case 'B': case 'b':
+			System.out.println("B 등급");
+			break;
+		default:
+			System.out.println("기타 등급");
+		}
+	}
+}
