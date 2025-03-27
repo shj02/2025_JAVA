c94e59bda8d3d78989c30ae3a811e8a702caab71
shj02 <163235571+shj02@users.noreply.github.com>
2025-03-27 14:36:42 +0900

    If, if else, else

---
+++ week04/IfElseErr.java
@@ -0,0 +1,26 @@
+package week4;
+
+public class IfElseErr {
+	public static void main(String[] args) {
+		int score = 85;
+		char grade;
+		
+		if (score >= 90) {
+			System.out.println("90 이상");
+			grade = 'A';
+		} else if (score >= 80) {
+			System.out.println("80 이상");
+			grade = 'B';
+		} else if (score >= 70) {
+			System.out.println("70 이상");
+			grade = 'C';
+		} else if (score >= 60) {
+			System.out.println("60 이상");
+			grade = 'D';
+		} else {
+			System.out.println("60 미만");
+			grade = 'F';
+		}
+		System.out.println(grade);
+	}
+}
