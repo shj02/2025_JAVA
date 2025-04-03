2e425c530879b52d85d4c9e9271ef2bd133d4f72
shj02 <163235571+shj02@users.noreply.github.com>
2025-04-03 14:14:57 +0900

    중첩for문

---
+++ week05/ForNested.java
@@ -0,0 +1,14 @@
+package week5;
+
+public class ForNested {
+
+	public static void main(String[] args) {
+		for(int i=0; i<3; i++) {
+			//i=0, 1, 2일 때 -> 3번 반복
+			for(int j=0; j<3; j++) {
+				//j=0, 1, 2
+				System.out.println("i="+i+", j="+j);
+			}
+		}
+	}
+}
