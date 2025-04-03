3a884e804e2cf716fde6a7d0895696e786c90919
shj02 <163235571+shj02@users.noreply.github.com>
2025-04-03 15:01:03 +0900

    While문 무한반복

---
+++ week05/WhileEx2.java
@@ -0,0 +1,25 @@
+package week5;
+
+import java.util.Scanner;
+
+public class WhileEx2 {
+	public static void main(String[] args) {
+		//while(true) : 무한 루프 이용
+		Scanner sc = new Scanner(System.in);
+		
+		int num1, num2;
+		
+		while(true) {
+			//무한 반복 부분
+			System.out.print("첫 번째 숫자 >> ");
+			num1 = sc.nextInt();
+			
+			System.out.print("두 번째 숫자 >> ");
+			num2 = sc.nextInt();
+			
+			System.out.printf("%d + %d = %d\n", num1, num2, num1+num2);
+		}
+		
+		//sc.close();
+	}
+}
