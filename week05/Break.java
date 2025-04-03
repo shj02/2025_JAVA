3e2a9d19f450893782db09508bb17d9303580cfc
shj02 <163235571+shj02@users.noreply.github.com>
2025-04-03 15:14:06 +0900

    Break

---
+++ week05/Break.java
@@ -0,0 +1,16 @@
+package week5;
+
+public class Break {
+	public static void main(String[] args) {
+		
+		while(true) {
+			int num = (int)(Math.random() * 10);
+			System.out.println("랜덤 숫자 : " + num);
+			
+			if(num == 7) {
+				System.out.println("프로그램 종료");
+				break;
+			}
+		}
+	}
+}
