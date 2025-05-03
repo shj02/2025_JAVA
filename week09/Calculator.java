dd9d7dc2e0ac487dbc87e546acc7529a38c23fb4
shj0218 <163235571+shj02@users.noreply.github.com>
2025-05-03 09:16:13 +0900

    메소드

---
+++ week09/Calculator.java
@@ -0,0 +1,29 @@
+package week8;
+
+public class Calculator {
+		String color;
+		String size;
+		
+		public Calculator(String color, String size) {
+			this.color;
+			this.size;
+		}
+		//전원 켜기
+		//리턴값이 없는 경우
+		void powerOn() {
+			System.out.println("전원을 켭니다.");
+		}
+		//전원 끄기
+		//리턴값이 없는 경우
+		void powerOff() {
+			System.out.println("전원을 끕니다.");
+		}
+		
+		int plus(int x, int y) {
+			return x+y;
+		}
+		
+		double divide(int x, int y) {
+			return (double)x/y;
+		}
+	}
