19f2defda3636e98835d07d7b74b8d20e360d5e8
shj0218 <163235571+shj02@users.noreply.github.com>
2025-05-03 10:43:53 +0900

    9주차 과제

--- week09/Student.java
+++ week09/Student.java
@@ -23,3 +23,16 @@ public class Student {
 		return (double)(kor + eng + math)/3;
 	}
 }
+
+//StudentEx.java
+package week8;
+
+public class StudentEx {
+	public static void main(String[] args) {
+		Student s1 = new Student("홍길동", 90, 85, 100);
+		
+		System.out.print(s1.name + " : ");
+		System.out.printf("총점 %d점, ", s1.getTotal());
+		System.out.printf("평균 %.1f점", s1.getAvg());
+	}
+}
