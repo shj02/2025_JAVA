ebf7de5daafe34461c32c800c7d978ca8f2b15ec
shj0218 <163235571+shj02@users.noreply.github.com>
2025-05-03 10:03:58 +0900

    Create Calculator2.java

---
+++ week09/Calculator2.java
@@ -0,0 +1,11 @@
+package week8;
+
+public class Calculator2 {
+	static double pi = 3.14;
+	static int plus(int x, int y) {
+		return x+y;
+	}
+	static int minus(int x, int y) {
+		return x-y;
+	}
+}
