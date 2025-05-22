9ae8fa0c605b1483cb3e27b228b8afea07058fb4
shj0218 <163235571+shj02@users.noreply.github.com>
2025-05-22 14:12:29 +0900

    Create Dog.java

---
+++ week11/Animal/Dog.java
@@ -0,0 +1,17 @@
+package week11;
+
+public class Dog extends Animal {
+	public Dog() {
+		this.kind = "포유류";
+	}
+	
+	@Override
+	public void sound() {
+		System.out.println("멍멍!");
+	}
+	
+	public void dogLife() {
+		System.out.println("Dog 평균 수명은 약 15~20년입니다.");
+	}
+	
+}
