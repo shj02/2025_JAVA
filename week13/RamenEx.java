84f47adc2e12c0fa3b6db477f2b2736af14abf98
shj0218 <163235571+shj02@users.noreply.github.com>
2025-05-29 15:06:12 +0900

    Create RamenEx.java

---
+++ week13/RamenEx.java
@@ -0,0 +1,14 @@
+package week13;
+
+public class RamenEx {
+	public static void main(String[] args) {
+		RamenCooker guest1 = new RamenCooker("홍길동");
+		guest1.start();
+		
+		RamenCooker guest2 = new RamenCooker("이순신");
+		guest2.start();
+		
+		RamenCooker guest3 = new RamenCooker("강감찬");
+		guest3.start();
+	}
+}
