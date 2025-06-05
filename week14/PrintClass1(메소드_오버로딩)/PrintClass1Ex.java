39527ea28aaf868804f593230fe6390fd8e2f77e
shj0218 <163235571+shj02@users.noreply.github.com>
2025-06-05 13:50:07 +0900

    Create PrintClass1Ex.java

---
+++ week14/PrintClass1(메소드_오버로딩)/PrintClass1Ex.java
@@ -0,0 +1,11 @@
+package week14;
+
+public class PrintClass1Ex {
+	public static void main(String[] args) {
+		PrintClass1 pc1 = new PrintClass1();
+		
+		pc1.printVal(100);
+		pc1.printVal("홍길동");
+		pc1.printVal(true);
+	}
+}
