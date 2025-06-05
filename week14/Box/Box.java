6707101355ae66f9422224e6c6bc574512545ca8
shj0218 <163235571+shj02@users.noreply.github.com>
2025-06-05 14:10:06 +0900

    Create Box.java(Object타입 선언)

---
+++ week14/Box/Box.java
@@ -0,0 +1,15 @@
+package week14;
+
+public class Box {
+	//모든 타입의 데이터를 저장하기 위해
+	//Object 타입으로 선언 = > 모든 클래스의 최상위 클래스
+	private Object obj;
+
+	public Object getObj() {
+		return obj;
+	}
+
+	public void setObj(Object obj) {
+		this.obj = obj;
+	}
+}
